25e3cef0df96f2b95ea702b68d0842f133915ace
Kirsten Green <hello@kirstengreen.com>
2021-02-09 13:32:52 -0500

    Defined players 1 and 2

--- connect-4-app/assets/scripts/app.js
+++ connect-4-app/assets/scripts/app.js
@@ -1,6 +1,31 @@
 
+////////////////////
+// DEFINE PLAYERS //
+////////////////////
+
+
+class Player {
+    constructor ( name, checker ) {
+        this.name = name;
+        this.checkerColor = checker;
+        this.moves = [];
+    }
+}
+
+
+const player1 = new Player( 'Player 1', 'red' );
+// console.log( player1 );
+const player2 = new Player( 'Player 2', 'yellow' );
+// console.log( player2 );
+
+
+
+////////////////
+// AFTER LOAD //
+////////////////
 
 $( () => {
+
 
     $( '.col' ).hover(
         function() {
